2e738eba520bae0fd9c180ac88466e87ecb297b5
Gabstc <70344987+Gabstc@users.noreply.github.com>
2020-09-09 14:55:51 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    volumeSphere = 4 / 3 * pi * radius ** 3
+    basic.showNumber(volumeSphere)
+})
+let volumeSphere = 0
+let pi = 0
+let radius = 0
+radius = 10
+pi = 3.1416
+basic.forever(function () {
+	
+})
